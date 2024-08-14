4b16f9523d015cf3409a28c97b2dce85c8f7a446
monukumar98 <monu012as@gmail.com>
2024-08-14 16:34:44 +0530

    Bst and BT

--- Lec2/Pattern14.java
+++ Lec2/Pattern14.java
@@ -4,7 +4,10 @@ public class Pattern14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 5;
+		int star = 1;
+		int row = 1;
+		int space=n-1;
 	}
 
 }
